67c6928c7eccf9c74d1816e26136631861e232ae
Abhishek Sharma <103446425+andysharn@users.noreply.github.com>
2024-09-13 16:50:19 +0530

    Update analyticRoutes.js

--- routes/analyticRoutes.js
+++ routes/analyticRoutes.js
@@ -2,9 +2,14 @@ const express = require('express');
 const { getAnalytics } = require('../controllers/analyticsController');
 const auth = require('../middlewares/authMiddleware');
 const rbac = require('../middlewares/rbacMiddleware');
-
+const rateLimit = require('express-rate-limit');
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100
+});
+app.use('/api/', apiLimiter);
 const router = express.Router();
 
-router.get('/analytics', auth, rbac(['Admin']), getAnalytics);
+router.get('/analytics', auth, rbac(['Admin']), getAnalytics,apiLimiter);
 
 module.exports = router;
