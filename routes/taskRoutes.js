d97fd5688095a32f20f7065cb7ad4af56ab4219c
Abhishek Sharma <103446425+andysharn@users.noreply.github.com>
2024-09-13 16:48:33 +0530

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,12 +2,16 @@ const express = require('express');
 const { createTask, getTasks, updateTask, deleteTask } = require('../controllers/taskController');
 const auth = require('../middlewares/authMiddleware');
 const rbac = require('../middlewares/rbacMiddleware');
-
+const rateLimit = require('express-rate-limit');
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100 
+});
 const router = express.Router();
 
-router.post('/tasks', auth, rbac(['Admin', 'Manager']), createTask);
-router.get('/tasks', auth, getTasks);
-router.put('/tasks/:taskId', auth, rbac(['Admin', 'Manager']), updateTask);
-router.delete('/tasks/:taskId', auth, rbac(['Admin']), deleteTask);
+router.post('/tasks', auth, rbac(['Admin', 'Manager']), createTask,apiLimiter);
+router.get('/tasks', auth, getTasks,apiLimiter);
+router.put('/tasks/:taskId', auth, rbac(['Admin', 'Manager']), updateTask),apiLimiter;
+router.delete('/tasks/:taskId', auth, rbac(['Admin']), deleteTask,apilimiter);
 
 module.exports = router;
