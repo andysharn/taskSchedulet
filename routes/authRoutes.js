67e279b8565e0bf994153c9ff4627d46f181209a
Abhishek Sharma <103446425+andysharn@users.noreply.github.com>
2024-09-13 16:58:59 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,14 +2,11 @@ const express = require('express');
 const { register, login, logout ,getProfile} = require('../controllers/authController');
 const auth = require('../middlewares/authMiddleware');
 const rateLimit = require('express-rate-limit');
-const apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100 
-});
+
 const router = express.Router();
 
-router.post('/register', register,apiLimiter);
-router.post('/login', login,apiLimiter);
-router.post('/logout', auth, logout,apiLimiter);
-router.get('/profile', auth, getProfile,apiLimiter); 
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', auth, logout);
+router.get('/profile', auth, getProfile); 
 module.exports = router;
