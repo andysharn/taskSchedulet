b10b35ef0090de265ff705fec871729d6987424b
Abhishek Sharma <103446425+andysharn@users.noreply.github.com>
2024-09-13 17:00:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const taskRoutes = require('./routes/taskRoutes');
 const analyticsRoutes = require('./routes/analyticRoutes');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
-
-
 const app = express();
+const rateLimit = require('express-rate-limit');
+
+
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100, 
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+});
+
+app.use('/api/', apiLimiter);
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
