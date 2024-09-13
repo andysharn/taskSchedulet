7372f8725307a537eccb08b46740233eb0bbc674
Abhishek Sharma <103446425+andysharn@users.noreply.github.com>
2024-09-13 16:37:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ const taskRoutes = require('./routes/taskRoutes');
 const analyticsRoutes = require('./routes/analyticRoutes');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
-const { swaggerUi, swaggerSpec } = require('./utils/swagger');
+
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api', authRoutes);
 app.use('/api', taskRoutes);
 app.use('/api', analyticsRoutes);
